4987b8674d0571e8955bf1354db4ce1e6b140cdd
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-12-06 17:04:58 +0330

    feat: add RenderRect
    this commit also updated the Renderer type to get an image.Rectangle instead of image.Image

--- render.go
+++ render.go
@@ -9,18 +9,23 @@ import (
 
 // Renderer is a function which gets the [image.Image] and the current [image.Point],
 // and returns a [image/color.RGBA] for that point.
-type Renderer func(img image.Image, point image.Point) color.RGBA
+type Renderer func(rect image.Rectangle, point image.Point) color.RGBA
 
 // Render calls the renderer in each pixel of the image
 // in NumCPU*2 goroutines for better performance.
 func Render(img *image.RGBA, renderer Renderer) {
+	RenderRect(img, img.Rect, renderer)
+}
+
+func RenderRect(img *image.RGBA, rect image.Rectangle, renderer Renderer) {
 	var wg sync.WaitGroup
 	var queue = make(chan int, runtime.NumCPU()*2)
 
 	rowGenerator := func() {
 		for y := range queue {
-			for x := 0; x < img.Rect.Dx(); x++ {
-				rc := renderer(img, image.Point{X: x, Y: y})
+
+			for x := rect.Min.X; x < rect.Max.X; x++ {
+				rc := renderer(rect, image.Point{X: x, Y: y})
 
 				img.SetRGBA(x, y, MergeRGBA(img.RGBAAt(x, y), rc))
 			}
@@ -33,7 +38,7 @@ func Render(img *image.RGBA, renderer Renderer) {
 		go rowGenerator()
 	}
 
-	for y := 0; y < img.Bounds().Dy(); y++ {
+	for y := rect.Min.Y; y < rect.Max.Y; y++ {
 		wg.Add(1)
 		queue <- y
 	}
