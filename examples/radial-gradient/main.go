e66fdddc220a242254ad7f62d0e9ed6d726a3e75
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-12-06 15:44:50 +0330

    feat: add a transparent gradient in RadialGradient example

--- examples/radial-gradient/main.go
+++ examples/radial-gradient/main.go
@@ -11,12 +11,15 @@ var (
 	Width, Height  = 1920, 1080
 	PrimaryColor   = color.RGBA{28, 181, 224, 255}
 	SecondaryColor = color.RGBA{0, 0, 70, 255}
+	TertiaryColor  = color.RGBA{48, 43, 99, 255}
 )
 
 func main() {
 	img := painter.New(Width, Height)
 
 	painter.Render(img, painter.RadialGradient(PrimaryColor, SecondaryColor, image.Point{1000, 350}))
+
+	painter.Render(img, painter.RadialGradient(TertiaryColor, color.RGBA{}, image.Point{1700, 950}))
 
 	err := painter.SavePNG(img, "examples/radial-gradient/radial-gradient.png")
 	if err != nil {
