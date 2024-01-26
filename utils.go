6d3109228369a36b2dbd5a8949eb1f2e60ea3f18
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-01-26 04:03:37 +0330

    chore: optimize clamp

--- utils.go
+++ utils.go
@@ -4,20 +4,24 @@ import (
 	"math"
 )
 
-// clamp returns the value of x constrained to the range minVal to maxVal.
-//
-// more: https://thebookofshaders.com/glossary/?search=clamp
-func clamp(x, minVal, maxVal float64) float64 {
-	return min(max(x, minVal), maxVal)
+// clamp returns the value of x constrained to the range lowerLimit to upperLimit.
+func clamp(x, lowerLimit, upperLimit float64) float64 {
+	if x < lowerLimit {
+		return lowerLimit
+	}
+	if x > upperLimit {
+		return upperLimit
+	}
+	return x
 }
 
 // smoothStep performs smooth Hermite interpolation between 0 and 1 when edge0 < x < edge1.
 // This is useful in cases where a threshold function with a smooth transition is desired.
 //
-// more: https://thebookofshaders.com/glossary/?search=smoothstep
+// Learn more: https://en.wikipedia.org/wiki/Smoothstep
 func smoothStep(edge0, edge1, x float64) float64 {
-	t := clamp((x-edge0)/(edge1-edge0), 0.0, 1.0)
-	return t * t * (3.0 - 2.0*t)
+	x = clamp((x-edge0)/(edge1-edge0), 0, 1)
+	return x * x * (3.0 - 2.0*x)
 }
 
 // getDistance returns the distance between two coordinates using the distance formula.
